5c68bcc517e004c5e80e3f4112d7eed53955fc82
Hossein Amiri <hossein.amiri.enterprise@gmail.com>
2021-08-20 22:53:57 +0430

    third commit

--- pages/index.js
+++ pages/index.js
@@ -1,136 +1,159 @@
-import { useState } from "react";
-import { Button, TimePicker } from "antd";
-import { GrPowerReset } from "react-icons/gr";
+import {useState} from "react";
+import {Button, TimePicker} from "antd";
+import {GrPowerReset} from "react-icons/gr";
 import moment from "moment";
 
 const IndexPage = () => {
-  const defaultTotalTime = moment("00:30:00", "HH:mm:ss");
-  const defaultStepTime = moment("00:01:00", "HH:mm:ss");
-  const defaultDelayTime = moment("00:0:05", "HH:mm:ss");
-  const referenceTime = moment("00:0:00", "HH:mm:ss");
+    const defaultTotalTime = moment("00:00:30", "HH:mm:ss");
+    const defaultStepTime = moment("00:00:10", "HH:mm:ss");
+    const defaultDelayTime = moment("00:0:05", "HH:mm:ss");
+    const referenceTime = moment("00:0:00", "HH:mm:ss");
 
-  const [totalTime, setTotalTime] = useState(defaultTotalTime);
-  const [stepTime, setStepTime] = useState(defaultStepTime);
-  const [delayTime, setDelayTime] = useState(defaultDelayTime);
+    const [totalTime, setTotalTime] = useState(defaultTotalTime);
+    const [stepTime, setStepTime] = useState(defaultStepTime);
+    const [delayTime, setDelayTime] = useState(defaultDelayTime);
 
-  const onSelectTotalTime = (time) => {
-    setTotalTime(time);
-  };
-  const onSelectStepTime = (time) => {
-    setStepTime(time);
-  };
-  const onSelectDelayTime = (time) => {
-    setDelayTime(time);
-  };
-  const resetTotalTime = () => {
-    setTotalTime(defaultTotalTime);
-  };
-  const resetStepTime = () => {
-    setStepTime(defaultStepTime);
-  };
-  const resetDelayTime = () => {
-    setDelayTime(defaultDelayTime);
-  };
+  let totalInterval = undefined;
+  let comonInterval = undefined;
+  let delayInterval = undefined;
 
-  const startCounting = () => {
-    const baseCountingTime = moment();
-    const baseTotalTime = totalTime;
-    const baseStepTime = stepTime;
-    const baseDelayTime = delayTime;
+    const resetTotalTimeTo = (time = defaultTotalTime) => {
+        setTotalTime(time);
+    };
+    const resetStepTimeTo = (time = defaultStepTime) => {
+        setStepTime(time);
+    };
+    const resetDelayTimeTo = (time = defaultDelayTime) => {
+        setDelayTime(time);
+    };
 
-    do {
-      countDownTotalTime(baseTotalTime, baseCountingTime);
-      countDownDelayAndStep(baseDelayTime, baseStepTime, baseCountingTime);
-    } while (totalTime.isBefore(moment().startOf("day")));
-  };
-  const countDownDelayAndStep = (
-    baseDelayTime,
-    baseStepTime,
-    baseCountingTime
-  ) => {
-    countDownDelayTime(baseDelayTime, baseCountingTime);
-    countDownStepTime(baseStepTime, baseCountingTime);
-  };
-  const countDownTotalTime = (baseTotalTime, baseCountingTime) => {
-    setInterval(() => {
-      setTotalTime(
-        moment(baseTotalTime.diff(moment(moment().diff(baseCountingTime))))
+    const resetAll = () => {
+        resetTotalTimeTo();
+        resetStepTimeTo();
+        resetDelayTimeTo();
+    }
+
+    const startCounting = async () => {
+        const now = moment();
+        const baseTotalTime = totalTime;
+        const baseStepTime = stepTime;
+        const baseDelayTime = delayTime;
+
+        await countDownTotalTime(totalTime, now);
+        await countDownStepTime(delayTime, stepTime, now);
+    };
+    const stopCounting = () => {
+        //reset timers
+        console.log("reset the timers")
+
+    }
+    const pauseCounting = () => {
+        console.log("pause the timers")
+    }
+
+    const countDownTotalTime = (baseTotalTime, baseCountingTime) => {
+      totalInterval = setInterval(() => {
+            const timer = moment(baseTotalTime.diff(moment(moment().diff(baseCountingTime))))
+            setTotalTime(timer);
+            if (timer < referenceTime) {
+                resetTotalTimeTo(baseTotalTime);
+                clearInterval(totalInterval)
+            }
+        }, 1000);
+    };
+
+
+
+    const countDownStepTime = (baseDelayTime, baseStepTime, baseCountingTime) => {
+      clearInterval(comonInterval);
+      console.log("in countDownStepTime", comonInterval)
+
+      comonInterval = setInterval(() => {
+            const timer = moment(
+                baseStepTime.diff(moment(moment().diff(baseCountingTime)))
+            );
+            setStepTime(timer);
+            if (timer<referenceTime){
+              setStepTime(baseStepTime);
+               countDownDelayTime(baseDelayTime, baseStepTime, baseCountingTime)
+
+            }
+
+        }, 1000);
+    };
+  const countDownDelayTime = (baseDelayTime, baseStepTime, baseCountingTime) => {
+    clearInterval(comonInterval)
+    console.log("in countDownDelayTime", comonInterval)
+    comonInterval =  setInterval(() => {
+      const timer = moment(
+          baseDelayTime.diff(moment(moment().diff(baseCountingTime)))
       );
-    }, 1000);
-  };
-  const countDownDelayTime = (baseDelayTime, baseCountingTime) => {
-    setInterval(() => {
-      const downCounter = moment(
-        baseDelayTime.diff(moment(moment().diff(baseCountingTime)))
-      );
-
-      setDelayTime(downCounter);
-      if (downCounter.isBefore(moment().startOf("day")))
+      setDelayTime(timer);
+      if (timer <referenceTime)
         setDelayTime(baseDelayTime);
-      return;
+
+       countDownStepTime(baseDelayTime, baseStepTime, baseCountingTime)
+
     }, 1000);
   };
+    //utils
+    const getMilSeconds = (moment) => {
+        return moment.valueOf() - referenceTime.valueOf();
+    };
 
-  const countDownStepTime = (baseStepTime, baseCountingTime) => {
-    setInterval(() => {
-      const downCounter = moment(
-        baseStepTime.diff(moment(moment().diff(baseCountingTime)))
-      );
-      setStepTime(downCounter);
-      if (downCounter.isBefore(moment().startOf("day")))
-        setStepTime(baseStepTime);
-      return;
-    }, 1000);
-  };
+    return (
+        <div className="container">
+            <div>
+                <p>{JSON.stringify(stepTime)}</p>
+                <TimePicker
+                    value={totalTime}
+                    placeholder="Total Time"
+                    onSelect={resetTotalTimeTo}
+                    clearIcon=""
+                    suffixIcon=""
+                />
+                <Button
+                    onClick={resetTotalTimeTo}
+                    style={{border: "none"}}
+                    icon={<GrPowerReset/>}
+                ></Button>
+                <TimePicker
+                    value={stepTime}
+                    placeholder="Step Time"
+                    onSelect={resetStepTimeTo}
+                    clearIcon=""
+                    suffixIcon=""
 
-  //utils
-  const getMilSeconds = (moment) => {
-    return moment.valueOf() - referenceTime.valueOf();
-  };
-  return (
-    <div className="container">
-      <div>
-        <p>{JSON.stringify(stepTime)}</p>
-        <TimePicker
-          value={totalTime}
-          placeholder="Total Time"
-          onSelect={onSelectTotalTime}
-          clearIcon=""
-          suffixIcon=""
-        />
-        <Button
-          onClick={resetTotalTime}
-          style={{ border: "none" }}
-          icon={<GrPowerReset />}
-        ></Button>
-        <TimePicker
-          value={stepTime}
-          placeholder="Step Time"
-          onSelect={onSelectStepTime}
-          clearIcon=""
-          suffixIcon=""
-        />
-        <Button
-          onClick={resetStepTime}
-          style={{ border: "none" }}
-          icon={<GrPowerReset />}
-        ></Button>
-        <TimePicker
-          value={delayTime}
-          placeholder="Delay Time"
-          onSelect={onSelectDelayTime}
-          clearIcon=""
-          suffixIcon=""
-        />
-        <Button
-          onClick={resetDelayTime}
-          style={{ border: "none" }}
-          icon={<GrPowerReset />}
-        ></Button>
-        <Button onClick={startCounting}> Start Counting</Button>
-      </div>
-    </div>
-  );
+                />
+                <Button
+                    onClick={resetStepTimeTo}
+                    style={{border: "none"}}
+                    icon={<GrPowerReset/>}
+                ></Button>
+                <TimePicker
+                    value={delayTime}
+                    placeholder="Delay Time"
+                    onSelect={resetDelayTimeTo}
+                    clearIcon=""
+                    suffixIcon=""
+                />
+                <Button
+                    onClick={resetDelayTimeTo}
+                    style={{border: "none"}}
+                    icon={<GrPowerReset/>}
+                ></Button>
+                <Button
+                    onClick={resetAll}
+                >Reset All</Button>
+
+            </div>
+            <div style={{display: "flex", alignItems: "center"}}>
+                <Button onClick={startCounting}> Start</Button>
+                <Button onClick={stopCounting}> Stop</Button>
+                <Button onClick={pauseCounting}> Pause</Button>
+            </div>
+        </div>
+    );
 };
 
 export default IndexPage;
